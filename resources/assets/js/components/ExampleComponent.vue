1b18653bccdb8e2224f482f5dcfab39d05cb66c6
MR <admin@mr-cn.net>
2018-04-04 21:08:38 +0800

    Initial Commit

---
+++ resources/assets/js/components/ExampleComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="uk-container">
+        <div class="uk-card uk-card-primary uk-card-body">
+            <h3 class="uk-card-title">Example Component</h3>
+            I'm an example component!
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
