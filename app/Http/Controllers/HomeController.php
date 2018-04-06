42156f5d345cf737135fa3a5ba97cc897bbb0ac0
MR <admin@mr-cn.net>
2018-04-06 21:31:18 +0800

    栏目列表完成

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
@@ -33,8 +35,21 @@ class HomeController extends Controller
 	 *
 	 * @return \Illuminate\Http\Response
 	 */
-	public function market()
+	public function market(Request $request)
 	{
-		return view('market')->with([]);
+		$tag        = $request->get('tag') ?: "全部";
+		$allTag     = Book::all()->pluck('tag');
+		$isErrorTag = (Book::where('tag', $tag)->count() == 0) && ($tag != "全部"); // 判断get提交的Tag是不是真的Tag
+		if ($tag == "全部" || $isErrorTag) { // 全部书籍 或者 输入了错误的标签
+			$books = Book::paginate(15);
+		} else {
+			$books = Book::where('tag', $tag)->paginate(15);
+		}
+		return view('market')->with([
+			'books'      => $books,
+			'isErrorTag' => $isErrorTag,
+			'allTag'     => $allTag,
+			'currentTag' => $tag
+		]);
 	}
 }
--- resources/views/market.blade.php
+++ resources/views/market.blade.php
@@ -11,61 +11,42 @@
             <div uk-grid>
                 <div class="uk-width-small uk-margin-top uk-margin-right">
                     <ul class="uk-nav uk-nav-default">
-                        <li class="uk-active"><a href="#">全部</a></li>
+                        <li{!! $currentTag == "全部" || $isErrorTag ? ' class="uk-active"' : "" !!}><a href="?">全部</a>
+                        </li>
                         <li class="uk-nav-divider"></li>
-                        <li><a href="#">科技</a></li>
-                        <li><a href="#">商业</a></li>
+                        @foreach($allTag as $tag)
+                            <li{!! $currentTag == $tag ? ' class="uk-active"' : "" !!}><a
+                                        href="?tag={{ $tag }}">{{ $tag }}</a></li>
+                        @endforeach
                     </ul>
                 </div>
                 <div class="uk-width-expand">
-                    <article class="uk-comment uk-margin-bottom">
-                        <header class="uk-comment-header uk-flex-middle">
-                            <h4 class="uk-comment-title uk-margin-remove"><a class="uk-link-reset" href="#">Solidot</a>
-                            </h4>
-                            <ul class="uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top">
-                                <li><span>科技</span></li>
-                                <li><a href="#">预览</a></li>
-                            </ul>
-                        </header>
-                        <div class="uk-comment-body">
-                            <p>Solidot是至顶网下的科技资讯网站，主要面对开源自由软件和关心科技资讯读者群，包括众多中国开源软件的开发者，爱好者和布道者。口号是“奇客的资讯，重要的东西”。</p>
-                            <a href="#" class="uk-button uk-button-primary">查看详情</a>
+                    @if($isErrorTag)
+                        <div class="uk-alert-danger" uk-alert>
+                            <p>你所选择的标签下没有书籍。</p>
                         </div>
-                    </article>
-                    <article class="uk-comment uk-margin-bottom">
-                        <header class="uk-comment-header uk-flex-middle">
-                            <h4 class="uk-comment-title uk-margin-remove"><a class="uk-link-reset" href="#">知乎每日精选</a>
-                            </h4>
-                            <ul class="uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top">
-                                <li><span>综合</span></li>
-                                <li><a href="#">预览</a></li>
-                            </ul>
-                        </header>
-                        <div class="uk-comment-body">
-                            <p>知乎日报是一款拥有千万用户的资讯类客户端，每日提供来自知乎社区的精选问答，还有国内一流媒体的专栏特稿。</p>
-                            <a href="#" class="uk-button uk-button-primary">查看详情</a>
-                        </div>
-                    </article>
-                    <ul class="uk-pagination" uk-margin>
-                        <li><a href="#"><span uk-pagination-previous></span></a></li>
-                        <li><a href="#">1</a></li>
-                        <li class="uk-disabled"><span>...</span></li>
-                        <li><a href="#">4</a></li>
-                        <li><a href="#">5</a></li>
-                        <li><a href="#">6</a></li>
-                        <li class="uk-active"><span>7</span></li>
-                        <li><a href="#">8</a></li>
-                        <li><a href="#">9</a></li>
-                        <li><a href="#">10</a></li>
-                        <li class="uk-disabled"><span>...</span></li>
-                        <li><a href="#">20</a></li>
-                        <li><a href="#"><span uk-pagination-next></span></a></li>
-                    </ul>
+                    @endif
+                    @foreach($books as $book)
+                        <article class="uk-comment uk-margin-bottom">
+                            <header class="uk-comment-header uk-flex-middle">
+                                <h4 class="uk-comment-title uk-margin-remove"><a class="uk-link-reset"
+                                                                                 href="#">{{ $book->name }}</a>
+                                </h4>
+                                <ul class="uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top">
+                                    <li><span>{{ $book->tag }}</span></li>
+                                    <li><a href="#">预览</a></li>
+                                </ul>
+                            </header>
+                            <div class="uk-comment-body">
+                                <p>{{ $book->description }}</p>
+                                <a href="#" class="uk-button uk-button-primary">查看详情</a>
+                            </div>
+                        </article>
+                    @endforeach
+
+                    {{ $books->links() }}
                 </div>
             </div>
         </div>
     </div>
-    @push('scripts')
-
-    @endpush
 @endsection
